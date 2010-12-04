74b33015de5955974fb46c18a2a8fbec14a21b30
gruvin <gruvin@f6e3ec8c-4711-3139-51d9-7b347ef5a272>
2010-12-04 22:49:52 +0000

    buildrelease updates. No longer call svn update before building. If that was my idea, then I regret it. :-P If not, then I dont like it.

--- trunk/winbuildrelease.bat
+++ trunk/winbuildrelease.bat
@@ -1,15 +1,14 @@
-svn update
 cd src
 make clean
 make EXT=STD BEEPER=BUZZER KEYRPT=DELAY
-mv er9x.hex ../gruvin9x-stock.hex
+mv gruvin9x.hex ../gruvin9x-stock.hex
 make clean  
 make 
-mv er9x.hex ../gruvin9x.hex
+mv gruvin9x.hex ../gruvin9x.hex
 make clean
 make BEEPER=SPEAKER KEYRPT=DELAY
-mv er9x.hex ../gruvin9x-std-speaker.hex
+mv gruvin9x.hex ../gruvin9x-std-speaker.hex
 make clean
 make EXT=FRSKY BEEPER=BUZZER KEYRPT=DELAY
-mv er9x.hex ../gruvin9x-frsky-nospeaker.hex
+mv gruvin9x.hex ../gruvin9x-frsky-nospeaker.hex
 make clean
