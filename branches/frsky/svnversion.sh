247a98b9d434d6ea6a455d642f05cb793e7233cf
gruvin <gruvin@f6e3ec8c-4711-3139-51d9-7b347ef5a272>
2011-06-29 03:04:17 +0000

    Committing latest .hex binary build

--- branches/frsky/svnversion.sh
+++ branches/frsky/svnversion.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-LASTREV=`cat .svn/entries | sed -n '4p'`
-THISREV=$(($LASTREV + 1))
 if [ -x /usr/local/bin/convert ]; then
-  svn info |\
-    sed "s/Revision: $LASTREV/Revision: $THISREV/g" |\
-    sed "s/Rev: $LASTREV/Rev: $THISREV/g" |\
-  /usr/local/bin/convert \
+  svn info | /usr/local/bin/convert \
     -background lightgrey -fill black \
     -pointsize 14 -font Courier \
     label:@- \
