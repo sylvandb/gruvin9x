55e08ca16db30cef32599a03adf37df04d596d31
gruvin <gruvin@f6e3ec8c-4711-3139-51d9-7b347ef5a272>
2011-08-18 01:05:55 +0000

    Experiemented with long filename support. But it added a whopping 64KB to the project! We have enough Flash for that and then some -- but it's crazy. Not going to do it.
    I didn't look in detail what the casue was -- but I think the unicode codepage
    conversion data was a large chunk of it.
    
    I've left the required support file (ffcc*.cpp) but commented it out in Makefile.

--- branches/frsky/src/ff.cpp
+++ branches/frsky/src/ff.cpp
@@ -3518,6 +3518,34 @@ FRESULT f_mkfs (
 
 #endif /* _USE_MKFS && !_FS_READONLY */
 
+#if _USE_LFN == 3 /* LFN with a working buffer on the heap */
+/*------------------------------------------------------------------------*/
+/* Allocate a memory block                                                */
+/*------------------------------------------------------------------------*/
+/* If a NULL is returned, the file function fails with FR_NOT_ENOUGH_CORE.
+   */
+
+void* ff_memalloc ( /* Returns pointer to the allocated memory block */
+      UINT size   /* Number of bytes to allocate */
+    )
+{
+    return malloc(size);
+}
+
+
+/*------------------------------------------------------------------------*/
+/* Free a memory block                                                    */
+/*------------------------------------------------------------------------*/
+
+void ff_memfree(
+      void* mblock  /* Pointer to the memory block to free */
+    )
+{
+    free(mblock);
+}
+
+#endif
+
 
 
 
