124f0af42a0430d1ceae2b6731ba76e28487faf1
gruvin <gruvin@f6e3ec8c-4711-3139-51d9-7b347ef5a272>
2011-09-14 11:36:44 +0000

    Fixed I2C RTC read functions and real time clock time advance for PCBV4.
    PPM_OUT *still* not working. *Still* no idea why. *Still* very annoyed about it. :/

--- branches/frsky/src/drivers.cpp
+++ branches/frsky/src/drivers.cpp
@@ -320,7 +320,7 @@ void per10ms()
     g_tmr10ms++;
     g_blinkTmr10ms++;
 
-#if defined (PCBV3) && !defined (PCBV4)
+#if defined (PCBV3)
     /* Update gloabal Date/Time every 100 per10ms cycles */
     if (++g_ms100 == 100)
     {
