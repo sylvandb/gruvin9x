aa4142167d99aaa598747507ad33386f91e051b7
bsongis <bsongis@f6e3ec8c-4711-3139-51d9-7b347ef5a272>
2011-10-02 21:27:17 +0000

    popMenu is always called without argument

--- branches/frsky/src/menus.h
+++ branches/frsky/src/menus.h
@@ -60,14 +60,13 @@ extern uint8_t g_menuStackPtr;
 void    chainMenu(MenuFuncP newMenu);
 /// goto given Menu, store current menu in menuStack
 void    pushMenu(MenuFuncP newMenu);
+/// return to last menu in menustack
+void    popMenu();
 ///deliver address of last menu which was popped from
 inline MenuFuncP lastPopMenu()
 {
   return g_menuStack[g_menuStackPtr+1];
 }
-/// return to last menu in menustack
-/// if uppermost is set true, thenmenu return to uppermost menu in menustack
-void    popMenu(bool uppermost=false);
 
 void menuMainView(uint8_t event);
 void menuProcSetup(uint8_t event);
