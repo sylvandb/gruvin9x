849952ea756ba2f5542b31001cb7428fb9bbd280
bsongis <bsongis@f6e3ec8c-4711-3139-51d9-7b347ef5a272>
2011-12-16 19:06:35 +0000

    Now it compiles without a warning under Visual C++ ... Why? Only because I wanted to have exceptions catched under Windows as we have on Linux. Most users are Windows. They will be all testers :)

--- branches/frsky/src/gruvin9x.cpp
+++ branches/frsky/src/gruvin9x.cpp
@@ -302,7 +302,7 @@ void applyExpos(int16_t *anas, uint8_t phase)
 }
 
 bool s_noStickInputs = false;
-inline int16_t __attribute__ ((always_inline)) getValue(uint8_t i)
+FORCEINLINE int16_t getValue(uint8_t i)
 {
     if(i<NUM_STICKS+NUM_POTS) return (s_noStickInputs ? 0 : calibratedStick[i]);
     else if(i<MIX_FULL/*srcRaw is shifted +1!*/) return 1024; //FULL/MAX
